fd0556c3153b713fc97f13bd9a722191d007c05c
jmund <jmundy2003@gmail.com>
2024-05-20 05:08:44 -0400

    * Added roll command and better logging

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+import random
 import discord
 import os
+
 import blackjack
+import responses
+from dice import DiceView
 
 bot = discord.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -10,7 +14,7 @@ async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 
-@bot.slash_command(name="Blackjack",
+@bot.slash_command(name="blackjack",
                    description="play a game of blackjack",
                    test_guild="1241262568014610482")
 async def blackjack_game_command(ctx: discord.ApplicationContext):
@@ -21,9 +25,26 @@ async def blackjack_game_command(ctx: discord.ApplicationContext):
     await view.start_game(ctx)
 
 
+@bot.slash_command(name="roll",
+                   description="roll a die",
+                   test_guild="1241262568014610482")
+async def roll_die(ctx: discord.ApplicationContext, sides: int = 6):
+    if sides < 1:
+        await ctx.respond("Die must have at least 1 side!")
+        return
+    await ctx.respond(f"Rolled a {random.randint(1, sides)}", view=DiceView(sides))
+
+
 @bot.listen()
 async def on_message(message):
+
+    if message.author == bot.user:
+        return
+
     print(f"Message sent in channel {message.channel}")
+    if message.content[0] == "!":
+        print(f"Command detected: {message.content}")
+        await responses.handle_response(message, bot)
 
 
 def run_bot():
