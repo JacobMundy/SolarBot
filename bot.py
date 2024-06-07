e3a67a11ce3306fbba5f63ee9fa0df3fae429a54
jmund <jmundy2003@gmail.com>
2024-06-07 08:53:45 -0400

    * Refactored some functions - on_command event now only prints to terminal if channel isn't blacklisted - error command now provides parameters in both console and response * blacklist_check created - will stop command execution if channel is found in blacklist

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
 import os
+from dotenv import load_dotenv
 from discord.ext import bridge
 from console_colors import FontColors
+from database import db_commands as database
+
+load_dotenv()
 
 # Add any cogs you want to load here,
 # should be names of the files in the cogs directory
@@ -20,10 +24,14 @@ async def on_ready():
 
 @bot.event
 async def on_command(ctx):
+    if database.is_blacklisted_channel(str(ctx.channel.id)):
+        return
+
     cog_name = ctx.command.cog_name if ctx.command.cog_name else "None"
     if cog_name == "Admin":
         print(f"{FontColors.OK_GREEN} "
-              f"Admin Command: {ctx.command} "
+              f"Admin Command requested: {ctx.command} \n"
+              f"Called by: {ctx.author} "
               f"{FontColors.END}")
     else:
         print(f"{FontColors.OK_BLUE} "
@@ -34,9 +42,19 @@ async def on_command(ctx):
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+        param_list = ctx.command.clean_params.keys()
+        params = ", ".join(param_list)
         print(f"{FontColors.WARNING} "
-              f"Missing required argument in command: {ctx.command}"
+              f"Missing required argument in command: {ctx.command}, arguments: {params}"
               f"{FontColors.END}")
+        await ctx.respond(f"You are missing a required argument! \n"
+                          f"Command Arguments: {str(params)}")
+
+
+@bot.check
+async def blacklist_check(ctx):
+    return (not database.is_blacklisted_channel(str(ctx.channel.id))
+            or str(ctx.command) == "unblacklist_channel")
 
 
 def run_bot():
