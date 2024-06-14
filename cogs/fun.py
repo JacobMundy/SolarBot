9ab7c6da83949d9f502c90c4d4ab428a4f5896ac
jmund <jmundy2003@gmail.com>
2024-06-14 09:27:16 -0400

    * Added bridge command - magic8ball - is a magic 8ball

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,6 @@
 import random
 
+import discord
 from discord.ext import commands, bridge
 
 
@@ -34,6 +35,34 @@ class Responses(commands.Cog):
         """
         response = random.choice(["Hello!", "Hi!", "Hey!", "Yo!", "Sup!"])
         await ctx.respond(response)
+
+
+    @bridge.bridge_command(name="magic8ball",
+                           aliases=["8ball", "8b", "m8ball", "eightball"],
+                           description="ask the magic 8 ball a question",
+                           test_guild="1241262568014610482")
+    async def magic8ball(self, ctx, *, question):
+        """
+        Responds with a random magic 8-ball response.
+        :param ctx:
+        :param question:
+        :return:
+        """
+        # responses https://en.wikipedia.org/wiki/Magic_8_Ball#Possible_answers
+        affirmative = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes definitely.",
+                       "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes",
+                       "Signs point to yes"]
+        non_committal = ["Reply hazy, try again", "Ask again later", "Better not tell you now",
+                         "Cannot predict now", "Concentrate and ask again"]
+        negative = ["Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+        response = random.choice(affirmative + non_committal + negative)
+
+        embed = discord.Embed(title="Magic 8 Ball :8ball:",
+                              color=discord.Color.blurple())
+        embed.add_field(name="Question:", value=question, inline=False)
+        embed.add_field(name="Answer:", value=response, inline=False)
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
