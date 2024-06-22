cd1936f7645b8d778104b6c1be9514b096a3e769
JacobMundy <jmundy2003@gmail.com>
2024-06-22 01:11:35 -0400

    * added very rudimentary slots game - should make better in the future, doesn't feel good right now

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -1,3 +1,6 @@
+import asyncio
+import random
+
 import discord
 from discord.ext import commands
 from discord.ext import bridge
@@ -44,6 +47,63 @@ class Gambling(commands.Cog):
                                        bet_amount=bet_amount)
         ctx = await ctx.respond("Starting a game of blackjack!", view=view)
         await view.start_game(ctx)
+
+    @bridge.bridge_command(name="slots",
+                             description="play a game of slots",
+                            test_guild="1241262568014610482")
+    async def slots_game_command(self, ctx: discord.ApplicationContext):
+        """
+        Starts a game of slots and responds with the game state and appropriate buttons.
+        :param ctx:
+        :param bet_amount:
+        :return:
+        """
+
+        database.create_user(str(ctx.author.id))
+
+        bet_amount = 100
+
+        if database.get_balance(str(ctx.author.id)) < bet_amount:
+            await ctx.respond("You don't have enough money to bet that amount! \n"
+                              "current balance: " + str(database.get_balance(str(ctx.author.id))))
+            return
+
+
+        # Define the symbols
+        symbols = ["🍎", "🍊", "🍋", "🍒", "🍇", "🍉", "🍓", "🍑", "🍈", "🍌", "🍐", "🍍"]
+        values = {"🍎": 10, "🍊": 20, "🍋": 30, "🍒": 40, "🍇": 50, "🍉": 60, "🍓": 70, "🍑": 80, "🍈": 90, "🍌": 100, "🍐": 110, "🍍": 120}
+
+
+        # Randomly select three symbols
+        slot_result = random.choices(symbols, k=3)
+
+        # Check if the selected symbols match any of the winning combinations
+        reward = 0
+        if len(set(slot_result)) == 1:
+            reward = values[slot_result[0]] * 100
+        elif len(set(slot_result)) == 2:
+            reward = values[slot_result[0]] * 10
+        elif len(set(slot_result)) == 3:
+            reward = values[slot_result[0]] * 3
+
+        # Display the slot machine result with changing symbols
+        slot_message = await ctx.respond(" ".join(random.choices(symbols, k=3)))
+        for _ in range(3):
+            await asyncio.sleep(1)
+            await slot_message.edit(content=" ".join(random.choices(symbols, k=3)))
+        await asyncio.sleep(0.5)
+        await slot_message.edit(content=" ".join(slot_result))
+
+        # Update the player's balance and display the reward
+        database.add_balance(str(ctx.author.id), reward)
+        await ctx.edit(f"{" ".join(slot_result)}\n You won {reward}! Your new balance is {database.get_balance(str(ctx.author.id))}")
+
+
+
+
+
+
+
 
 
 def setup(bot):
