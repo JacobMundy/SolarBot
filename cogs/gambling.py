300aff88de1192d9bd9059897d88d0c568d59636
JacobMundy <jmundy2003@gmail.com>
2024-06-24 12:21:11 -0400

    * slots logic moved from gambling.py to its own logic file - to run slots now just the view update the message and use the play_slots function - after that the logic file will handle everything

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -1,16 +1,11 @@
-import asyncio
-import random
-from itertools import cycle
-
 import discord
 from discord.ext import commands
 from discord.ext import bridge
 from database import db_commands as database
 from game_logic import blackjack
+from game_logic import slots
 
-
-# TODO: move the gambling commands to a separate cog (blackjack)
-#       add more gambling commands like slots, roulette, etc.
+# TODO: add more gambling commands like roulette, etc.
 #       add an economy leaderboard (this would probably be in the banking cog)
 
 
@@ -49,7 +44,6 @@ class Gambling(commands.Cog):
         ctx = await ctx.respond("Starting a game of blackjack!", view=view)
         await view.start_game(ctx)
 
-    # TODO: add a view like blackjack so the game can be restarted without a new command
     @bridge.bridge_command(name="slots",
                              description="play a game of slots",
                             test_guild="1241262568014610482")
@@ -59,71 +53,13 @@ class Gambling(commands.Cog):
         :param ctx:
         :return:
         """
-
-        database.create_user(str(ctx.author.id))
-
+        # At some point if we want to add a bet amount to slots, we can add it to the params
+        # and then change the reward amount based on bets
         bet_amount = 200
 
-        if database.get_balance(str(ctx.author.id)) < bet_amount:
-            await ctx.respond(f"You don't have enough money to play slots ({bet_amount})!  \n"
-                              "current balance: " + str(database.get_balance(str(ctx.author.id))))
-            return
-
-
-        # Define the symbols
-        symbols = ["🍎", "🍊", "🍋", "🍒", "🍇", "🍉", "🍓", "🍑", "🍈", "🍌", "🍐", "🍍"]
-
-        # Create a single message for the slot machine
-        slot_message = await ctx.respond("Starting slots!")
-
-        # Create three separate cycles for each wheel of the slot machine
-        wheels = [cycle(random.sample(symbols, len(symbols))) for _ in range(3)]
-
-        # Create a list of lists to store the symbols of each wheel
-        wheel_symbols = [[next(wheel) for _ in range(3)] for wheel in wheels]
-
-        # 0 = all wheels spinning, 1 = first wheel stopped,
-        # 2 = second wheel stopped, 3 = error (so don't do that)
-        async def loop_for_wheels(number_of_wheels: int):
-            for _ in range(random.randint(3, 5)):
-                for i in range(number_of_wheels, 3):
-                    wheel_symbols[i].pop(0)
-                    wheel_symbols[i].append(next(wheels[i]))
-
-                await asyncio.sleep(0.5)
-                wheel_strings = (f"==========" + "\n" +
-                                 wheel_symbols[0][2] + "  " + wheel_symbols[1][2] + "  " + wheel_symbols[2][2] + "\n" +
-                                 wheel_symbols[0][1] + "  " + wheel_symbols[1][1] + "  " + wheel_symbols[2][1] + "\n" +
-                                 wheel_symbols[0][0] + "  " + wheel_symbols[1][0] + "  " + wheel_symbols[2][0] + "\n" +
-                                 "==========" + "\n\n" +
-                                 "Slots are spinning...")
-                await slot_message.edit(content=" ".join(wheel_strings))
-
-        await loop_for_wheels(0)
-        await loop_for_wheels(1)
-        await loop_for_wheels(2)
-
-        # Calculate the reward based on the final symbols
-        final_symbols = [wheel[1] for wheel in wheel_symbols]
-        reward = sum([symbols.index(symbol) * 10 for symbol in final_symbols])
-
-        # If symbols are the same, multiply the reward (a set cant have two of the same elements)
-        if final_symbols[0] == final_symbols[1] == final_symbols[2]:
-            reward *= 50
-        elif len(set(final_symbols)) < 3:
-            reward *= 10
-
-        # Update the player's balance and display the reward
-        database.add_balance(str(ctx.author.id), reward)
-
-        final_string = (f"==========" + "\n" +
-                         wheel_symbols[0][2] + "  " + wheel_symbols[1][2] + "  " + wheel_symbols[2][2] + "\n" +
-                         wheel_symbols[0][1] + "  " + wheel_symbols[1][1] + "  " + wheel_symbols[2][1] + "\n" +
-                         wheel_symbols[0][0] + "  " + wheel_symbols[1][0] + "  " + wheel_symbols[2][0] + "\n" +
-                         "==========" + "\n\n" +
-                         f"Slots Over! You won {reward}!")
-
-        await slot_message.edit(content=" ".join(final_string))
+        view = slots.SlotsView(ctx, bet_amount)
+        await view.update_message("Starting a game of slots!")
+        await view.play_slots()
 
 
 def setup(bot):
